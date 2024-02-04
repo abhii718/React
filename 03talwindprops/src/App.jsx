005d168e9f0fcc7df7fb8c09e64b62fcc8388da3
abhii718 <abhinaysingh47639@gamil.com>
2024-02-04 15:38:18 +0530

    React tailwind project

--- 03talwindprops/src/App.jsx
+++ 03talwindprops/src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./component/card";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  let myObj = {
+    username: "abhi",
+    age: 21,
+  };
   return (
     <>
-    <h1 className='bg-green-400'>Tailwind test</h1>
+      <h1 className="bg-green-400 text-black p-9 rounded-xl mb-4">
+        Tailwind test
+      </h1>
+      <Card username="abhinay" btnText = "click me" />
+      <Card username="raghav" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
