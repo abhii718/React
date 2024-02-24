47203f29bf294bf16c3486fbb9ea73d8a50b8a6f
abhii718 <abhinaysingh47639@gamil.com>
2024-02-24 17:23:06 +0530

    h

---
+++ reyansh/my-new-project2/app/layout.js
@@ -0,0 +1,10 @@
+import "./globals.css";
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="p-12">{children}</body>
+    </html>
+  );
+}
